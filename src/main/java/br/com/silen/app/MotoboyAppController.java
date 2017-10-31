57bd57462516770a3c663b35d339b087ab959e77
Ramon Andrade <andrade.rmn@gmail.com>
2017-10-31 20:15:29 -0200

    Adiciona controller para iniciar e finalizar entrega no app

--- src/main/java/br/com/silen/app/MotoboyAppController.java
+++ src/main/java/br/com/silen/app/MotoboyAppController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.composed.web.Get;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import br.com.silen.entregas.Entrega;
 import br.com.silen.entregas.EntregaRepository;
@@ -23,5 +24,23 @@ public class MotoboyAppController {
 		List<Entrega> entregas = entregaRepository.findAllByMotoboyUserId(userId);
 		
 		return entregas;
+	}
+		
+	@App
+	@Get("/app/entrega/{id}/start")
+	public void startEntrega(@PathVariable Long id) {
+		Entrega entrega = entregaRepository.findById(id).get();
+		
+		entrega.markAsStarted();
+		entregaRepository.save(entrega);			
+	}
+	
+	@App
+	@Get("/app/entrega/{id}/finish")
+	public void finishEntrega(@PathVariable Long id) {
+		Entrega entrega = entregaRepository.findById(id).get();
+		
+		entrega.markAsFinished();
+		entregaRepository.save(entrega);
 	}
 }
